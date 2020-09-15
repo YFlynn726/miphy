c4f68ec4cc5908408822af6817c98d1c1364855e
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-09-15 13:40:17 -0400

    added api and some search functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,35 +7,34 @@ import Landing from "../Landing/landing";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { data: false };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = { data: false };
+  // }
 
-  componentDidMount() {
-    fetch("https://api.imgflip.com/get_memes", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        this.setState({
-          data: data.data.memes,
-        });
-        console.log(this.state.data);
-      })
-      .catch((error) => {
-        console.log("Request failure: ", error);
-      });
-  }
+  // componentDidMount() {
+  //   fetch("https://api.imgflip.com/get_memes", {
+  //     method: "GET",
+  //     headers: {
+  //       Accept: "application/json",
+  //     },
+  //   })
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       this.setState({
+  //         data: data.data.memes,
+  //       });
+  //       console.log(this.state.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log("Request failure: ", error);
+  //     });
+  // }
 
   render() {
-    console.log(this.state.data[0]);
     return (
       <React.Fragment>
         <Router>
--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -2,53 +2,127 @@ import React, { Component } from "react";
 //import "../App.css";
 
 export default class Search extends Component {
-  render() {
-    return (
-      <div className="Search">
-        <h2>Search Meme</h2>
-        <form className="search-form">
-          <input type="text" name="search" className="search-input" />
-          <button type="submit" className="submit-btn">
-            Search
-          </button>
-        </form>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      isLoaded: false,
+      searchMeme: "",
+    };
   }
-}
 
-function search() {
-  const gallery = document.querySelector(".gallery");
-  const searchInput = document.querySelector(".search-input");
-  const submitButton = document.querySelector(".submit-btn");
-  let searchValue;
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+    console.log(this.state.searchMeme);
+    // const { searchedText } = event.target;
+    // console.log(searchedText.value);
+  };
 
-  async function memeImages() {
-    const dataFetch = await fetch("https://api.imgflip.com/get_memes", {
+  handleInputChange = (event) => {
+    this.setState({
+      searchMeme: event.target.value,
+    });
+    console.log(event.target.value);
+  };
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes", {
       method: "GET",
       headers: {
         Accept: "application/json",
       },
-    });
-    const data = await dataFetch.json();
-    console.log(data);
-    data.data.memes.forEach((meme) => {
-      console.log(meme);
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          isLoaded: true,
+          items: data.data.memes,
+        });
+        console.log(this.state.items);
+      })
+      .catch((error) => {
+        console.log("Request failure: ", error);
+      });
   }
-  memeImages();
-  return (
-    <div className="App">
-      <h2>Miphy</h2>
-      <form className="search-form">
-        <input type="text" name="search" className="search-input" />
-        <button type="submit" className="submit-btn">
-          Search
-        </button>
-      </form>
-      <main>
-        <div className="gallery"></div>
-      </main>
-    </div>
-  );
+  render() {
+    let filteredMemes = this.state.items.filter((item) => {
+      return item.name
+        .toLowerCase()
+        .includes(this.state.searchMeme.toLowerCase());
+    });
+    console.log(filteredMemes);
+    let { items, isLoaded } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="Search">
+          <h2>Search Meme</h2>
+          <form
+            className="search-form"
+            onSubmit={(event) => this.handleSubmit(event)}
+          >
+            <input
+              type="text"
+              name="search"
+              // value={this.state.searchMeme}
+              onChange={this.handleInputChange}
+              className="search-input"
+            />
+            <button type="submit" className="submit-btn">
+              Search
+            </button>
+          </form>
+
+          <div className="gallery">
+            {items.map((item) => (
+              <img key={item.id} src={item.url} alt="" />
+            ))}
+          </div>
+          <div>
+            {filteredMemes.map((item) => (
+              <img key={item.id} src={item.url} alt="" />
+            ))}
+          </div>
+        </div>
+      );
+    }
+  }
 }
+
+// function search() {
+//   const gallery = document.querySelector(".gallery");
+//   const searchInput = document.querySelector(".search-input");
+//   const submitButton = document.querySelector(".submit-btn");
+//   let searchValue;
+
+//   async function memeImages() {
+//     const dataFetch = await fetch("https://api.imgflip.com/get_memes", {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//       },
+//     });
+//     const data = await dataFetch.json();
+//     console.log(data);
+//     data.data.memes.forEach((meme) => {
+//       console.log(meme);
+//     });
+//   }
+//   memeImages();
+//   return (
+//     <div className="App">
+//       <h2>Miphy</h2>
+//       <form className="search-form">
+//         <input type="text" name="search" className="search-input" />
+//         <button type="submit" className="submit-btn">
+//           Search
+//         </button>
+//       </form>
+//       <main>
+//         <div className="gallery"></div>
+//       </main>
+//     </div>
+//   );
+// }
