55d1eb9e13f7afc13a64bd9b384e7c7cbdc48572
Supreet <supreet.kaur@live.com>
2020-12-03 16:25:53 -0500

    Removed comments, prep for deploying

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,33 +6,6 @@ import Landing from "../Landing/landing";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = { data: false };
-  // }
-
-  // componentDidMount() {
-  //   fetch("https://api.imgflip.com/get_memes", {
-  //     method: "GET",
-  //     headers: {
-  //       Accept: "application/json",
-  //     },
-  //   })
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       this.setState({
-  //         data: data.data.memes,
-  //       });
-  //       console.log(this.state.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log("Request failure: ", error);
-  //     });
-  // }
-
   render() {
     return (
       <React.Fragment>
